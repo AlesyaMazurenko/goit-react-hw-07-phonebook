4c3940c0f3901268df72f5c7ff95e52c714e91b5
AlesyaMazurenko <97690121+AlesyaMazurenko@users.noreply.github.com>
2022-12-11 19:51:43 +0100

    Update contacts-selector.js

--- src/redux/contacts/contacts-selector.js
+++ src/redux/contacts/contacts-selector.js
@@ -1,18 +1,21 @@
 import { getFilter } from "redux/filter/filter-selectors";
 
-export const getContacts = store => store.contacts;
+export const getContacts = state => state.contacts.items;
+export const selectIsLoading = state => state.contacts.isLoading;
+export const selectError = state => state.contacts.error;
 
 export const getFilteredContacts = state => {
 
     const contacts = getContacts(state);
     const filter = getFilter(state);
 
-    if (!filter) {
-      return contacts;
+  if (!filter) {
+    console.log(contacts);
+      return contacts.items;
     }
 
     const normalizeFilter = filter.toLowerCase();
-    const filteredContacts = contacts.filter(({ name, number }) => {
+    const filteredContacts = contacts.items.filter(({ name, number }) => {
       const normalizeName = name.toLowerCase();
       const result = normalizeName.includes(normalizeFilter) || number.includes(normalizeFilter);
       return result;
