4f735944369dd84935cbb2867a4b7715714f5367
AlesyaMazurenko <97690121+AlesyaMazurenko@users.noreply.github.com>
2022-12-13 01:15:06 +0100

    Update contacts.js

--- src/shared/api/contacts.js
+++ src/shared/api/contacts.js
@@ -1,23 +1,23 @@
 import axios from "axios";
 
 const instanceContacts = axios.create({
-    baseURL: "https://6391517b65ff4183112ad2e0.mockapi.io/api/contacts",
-    params: {
-        _limit: 25,
-    }
+    baseURL: "https://6391517b65ff4183112ad2e0.mockapi.io/api/",
+    // params: {
+    //     _limit: 25,
+    // }
 });
 
 export const getContactsA = async () => {
-    const { data } = await instanceContacts.get("/");
-    return data;
+    const response = await instanceContacts.get('/contacts');
+    return response.data;
 }
 
 export const addContactA = async (data) => {
-    const { data: result } = await instanceContacts.post("/", data);
-    return result;
+    const response = await instanceContacts.post('/contacts', data);
+    return response.data;
 }
 
 export const removeContactA = async (id) => {
-    const { data } = await instanceContacts.delete(`/${id}`);
-    return data;
+    const response = await instanceContacts.delete(`/contacts/${id}`);
+    return response.data;
 }
